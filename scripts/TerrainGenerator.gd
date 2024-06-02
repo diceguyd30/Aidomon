4652a57bddcff1638f49165ed61309024e873c1d
diceguyd30 <michael@ennismightier.com>
2024-06-01 18:20:17 -0700

    Fixed the spawn location of the creatures

--- scripts/TerrainGenerator.gd
+++ scripts/TerrainGenerator.gd
@@ -5,7 +5,7 @@ const CHARACTER = preload("res://scenes/character.tscn")
 @onready var timer = $Timer
 @onready var background = %Background
 
-var distance_spawned_offscreen = 100
+var off_screen_distance = 100
 
 func _on_timer_timeout():
 	var character = CHARACTER.instantiate()
@@ -14,7 +14,6 @@ func _on_timer_timeout():
 	timer.wait_time = randi_range(8, 20)
 
 func get_offscreen_spawnpoint() -> Vector2:
-	var rng := RandomNumberGenerator.new()
-	rng.randomize()
-	var screensize = get_viewport().size
-	return Vector2(screensize.x * 2 + distance_spawned_offscreen, randi_range(0 + 50, background.size.y - 50))
+	var x = background.size.x + off_screen_distance
+	var y = randi_range(0 + 50, background.size.y - 50)
+	return Vector2(x, y)
