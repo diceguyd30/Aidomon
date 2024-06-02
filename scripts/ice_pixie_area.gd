c036ebea06d5655b0d87be4b5f5a89c1cc99311d
diceguyd30 <michael@ennismightier.com>
2024-06-01 20:34:35 -0700

    Fixed the issue of the input event happening twice

--- scripts/ice_pixie_area.gd
+++ scripts/ice_pixie_area.gd
@@ -6,7 +6,7 @@ signal clicked
 func _process(delta):
 	ice_pixie.position.x -= 5 * delta
 
-func _on_input_event(_viewport, _event, _shape_idx):
-	if Input.is_action_just_pressed("click"):
+func _on_input_event(_viewport, event, _shape_idx):
+	if Input.is_action_just_pressed("click", true) and event.is_pressed() and not event.is_echo():
 		print("You Clicked Me!")
 		emit_signal("clicked")
