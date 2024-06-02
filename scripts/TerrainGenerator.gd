e7ae20f66729d7dba4e1adc5f2094bbe249dfeed
diceguyd30 <michael@ennismightier.com>
2024-06-01 17:43:02 -0700

    Added initial terrain generator
    Fixes $11a

---
+++ scripts/TerrainGenerator.gd
@@ -0,0 +1,21 @@
+class_name TerrainGenerator
+extends Node
+
+const CHARACTER = preload("res://scenes/character.tscn")
+@onready var timer = $Timer
+@onready var background = %Background
+
+var distance_spawned_offscreen = 100
+
+func _on_timer_timeout():
+	var character = CHARACTER.instantiate()
+	character.position = get_offscreen_spawnpoint()
+	background.add_child(character)
+	timer.wait_time = randi_range(8, 20)
+
+func get_offscreen_spawnpoint() -> Vector2:
+	var rng := RandomNumberGenerator.new()
+	rng.randomize()
+	var screensize = get_viewport().size
+	print(screensize)
+	return Vector2(screensize.x * 2 + distance_spawned_offscreen, randi_range(0 + 50, background.size.y - 50))
--- scripts/ice_pixie_area.gd
+++ scripts/ice_pixie_area.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
 signal clicked
+@onready var ice_pixie = %ice_pixie
+
+func _process(delta):
+	ice_pixie.position.x -= 5 * delta
 
 func _on_input_event(_viewport, _event, _shape_idx):
 	if Input.is_action_just_pressed("click"):
