175df246ba6aa9126dae21ab0c44dad347c51e6f
diceguyd30 <michael@ennismightier.com>
2024-06-25 03:49:02 -0700

    Experimenting with a new environment generator that can load elements from a constructor parameter

---
+++ scripts/environment_item_generator.gd
@@ -0,0 +1,7 @@
+class_name EnvironmentItemGenerator
+extends Object
+
+var environment_items: Array[EnvironmentItem]
+
+func _init(env_items: Array[EnvironmentItem] = []):
+	self.environment_items = env_items
