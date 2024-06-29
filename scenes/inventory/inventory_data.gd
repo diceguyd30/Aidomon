19b501314918a5d28fb5bac983cfd77235a4b90d
diceguyd30 <michael@ennismightier.com>
2024-06-29 08:47:50 -0700

    Restructure the inventory data resource file

--- scenes/inventory/inventory_data.gd
+++ scenes/inventory/inventory_data.gd
@@ -2,16 +2,12 @@ class_name InventoryData
 extends Resource
 
 var max_inventory_size: int = 10
-
 # Ordered inventory items as they should appear in the UI
 var inventory_items: Array[ItemStack] = []
+# Dictionary<ItemID: _InventoryMetadata>
+var inventory_metadata_map: Dictionary = {}
 
 class _InventoryMetadata:
 	var item_count: int = 0
 	# Dictionary<inventory_index: true>  Used as a Set
 	var index_map: Dictionary
-	
-# Dictionary<ItemID: _InventoryMetadata>
-var inventory_metadata_map: Dictionary = {}
-
-
