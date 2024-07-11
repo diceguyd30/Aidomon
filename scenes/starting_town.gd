77cfe953509e6364b257227b3f0f9861edf29929
diceguyd30 <michael@ennismightier.com>
2024-07-10 20:42:15 -0700

    Changed the interactable wilderness section to a texture button
    It makes it much more reliable and gives a lot of things for free like hover and pressed/toggle signals as well as alternating images based on hover and press.

--- scenes/starting_town.gd
+++ scenes/starting_town.gd
@@ -1,29 +1,14 @@
 class_name StartingTown
 extends Control
 
-@onready var wilderness_outline: ColorRect = %WildernessOutline
 @onready var wilderness_label: Label = %WildernessLabel
 @onready var wilderness_panel: PanelContainer = %WildernessPanel
 
-func _on_wilderness_mouse_entered() -> void:
-	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
-	_update_opacity(1.0)
+func _on_wilderness_btn_mouse_entered() -> void:
+	wilderness_label.modulate.a = 1.0
 
-func _on_wilderness_mouse_exited() -> void:
-	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
-	_update_opacity(.7)
+func _on_wilderness_btn_mouse_exited() -> void:
+	wilderness_label.modulate.a = .7
 
-func _update_opacity(opacity_: float) -> void:
-	wilderness_label.modulate.a = opacity_
-	var shader: ShaderMaterial = wilderness_outline.material
-	var color: Color = shader.get_shader_parameter("color")
-	color.a = opacity_
-	shader.set_shader_parameter("color", color)
-
-
-func _on_wilderness_input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
-	if event is InputEventMouseMotion:
-		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-			print("better")
-		if Input.is_action_just_pressed("click"):
-			wilderness_panel.visible = ! wilderness_panel.visible
+func _on_wilderness_btn_toggled(toggled_on: bool) -> void:
+	wilderness_panel.visible = toggled_on
