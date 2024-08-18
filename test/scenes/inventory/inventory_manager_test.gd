569e22cf3bb26aacd820667b875a335c4774ceb5
diceguyd30 <michael@ennismightier.com>
2024-08-18 11:56:34 -0700

    Fixed broken test

--- test/scenes/inventory/inventory_manager_test.gd
+++ test/scenes/inventory/inventory_manager_test.gd
@@ -12,7 +12,7 @@ var inventory_manager: InventoryManager
 func test_add_item_stack() -> void:
 	self.inventory_manager = auto_free(InventoryManager.new().with_inventory(null))
 	var item_stack: ItemStack = InventoryHelpers.build_test_item_stack()
-	assert_that(self.inventory_manager.add_item_stack(item_stack)).is_null()
+	assert_that(self.inventory_manager._add_item_stack(item_stack)).is_null()
 	assert_that(self.inventory_manager.inventory_data.inventory_items[0].count) \
 			.is_equal(InventoryHelpers.DEFAULT_STACK_COUNT)
 	assert_that(self.inventory_manager.inventory_data.inventory_metadata_map[InventoryHelpers.DEFAULT_ID] \
@@ -21,7 +21,7 @@ func test_add_item_stack() -> void:
 func test_empty_list_updated_on_add() -> void:
 	self.inventory_manager = auto_free(InventoryManager.new().with_inventory(null))
 	var item_stack: ItemStack = InventoryHelpers.build_test_item_stack()
-	assert_that(self.inventory_manager.add_item_stack(item_stack)).is_null()
+	assert_that(self.inventory_manager._add_item_stack(item_stack)).is_null()
 	assert_that(self.inventory_manager.inventory_data.inventory_metadata_map[null] \
 			.index_map.keys().size()).is_equal(InventoryHelpers.DEFAULT_MAX_STACK_SIZE - 1)
 
@@ -33,8 +33,8 @@ func test_add_item_when_inventory_full() -> void:
 	const TEST_ID_2: int = 2
 	var item_stack_1: ItemStack = InventoryHelpers.build_test_item_stack(TEST_ID_1)
 	var item_stack_2: ItemStack = InventoryHelpers.build_test_item_stack(TEST_ID_2)
-	assert_that(self.inventory_manager.add_item_stack(item_stack_1)).is_null()
-	assert_that(self.inventory_manager.add_item_stack(item_stack_2)).is_equal(item_stack_2)
+	assert_that(self.inventory_manager._add_item_stack(item_stack_1)).is_null()
+	assert_that(self.inventory_manager._add_item_stack(item_stack_2)).is_equal(item_stack_2)
 	assert_that(self.inventory_manager.inventory_data.inventory_items[0].count) \
 			.is_equal(InventoryHelpers.DEFAULT_STACK_COUNT)
 	assert_that(self.inventory_manager.inventory_data.inventory_items[0].item.id) \
@@ -46,13 +46,13 @@ func test_add_item_that_overflows_inventory() -> void:
 	self.inventory_manager = auto_free(InventoryManager.new().with_inventory(inventory_data))
 	var item_stack: ItemStack = InventoryHelpers.build_test_item_stack()
 	item_stack.count = 15
-	assert_that(self.inventory_manager.add_item_stack(item_stack).count).is_equal(5)
+	assert_that(self.inventory_manager._add_item_stack(item_stack).count).is_equal(5)
 
 func test_add_greater_than_stack_size() -> void:
 	self.inventory_manager = auto_free(InventoryManager.new().with_inventory(null))
 	var item_stack: ItemStack = InventoryHelpers.build_test_item_stack()
 	item_stack.count = InventoryHelpers.DEFAULT_MAX_STACK_SIZE * 3
-	assert_that(self.inventory_manager.add_item_stack(item_stack)).is_null()
+	assert_that(self.inventory_manager._add_item_stack(item_stack)).is_null()
 	assert_that(self.inventory_manager.inventory_data.inventory_items[2].count) \
 			.is_equal(InventoryHelpers.DEFAULT_MAX_STACK_SIZE)
 	assert_that(self.inventory_manager.inventory_data.inventory_metadata_map[InventoryHelpers.DEFAULT_ID] \
@@ -61,8 +61,8 @@ func test_add_greater_than_stack_size() -> void:
 func test_update_existing_item_stack() -> void:
 	self.inventory_manager = auto_free(InventoryManager.new().with_inventory(null))
 	var item_stack: ItemStack = InventoryHelpers.build_test_item_stack()
-	assert_that(self.inventory_manager.add_item_stack(item_stack)).is_null()
-	assert_that(self.inventory_manager.add_item_stack(item_stack)).is_null()
+	assert_that(self.inventory_manager._add_item_stack(item_stack)).is_null()
+	assert_that(self.inventory_manager._add_item_stack(item_stack)).is_null()
 	assert_that(self.inventory_manager.inventory_data.inventory_items[0].count) \
 			.is_equal(InventoryHelpers.DEFAULT_STACK_COUNT * 2)
 
@@ -70,8 +70,8 @@ func test_update_item_stack_overflow() -> void:
 	self.inventory_manager = auto_free(InventoryManager.new().with_inventory(null))
 	var item_stack: ItemStack = InventoryHelpers.build_test_item_stack()
 	item_stack.item.max_stack_size = InventoryHelpers.DEFAULT_STACK_COUNT
-	assert_that(self.inventory_manager.add_item_stack(item_stack)).is_null()
-	assert_that(self.inventory_manager.add_item_stack(item_stack)).is_null()
+	assert_that(self.inventory_manager._add_item_stack(item_stack)).is_null()
+	assert_that(self.inventory_manager._add_item_stack(item_stack)).is_null()
 	assert_that(self.inventory_manager.inventory_data.inventory_items[1].count) \
 			.is_equal(InventoryHelpers.DEFAULT_STACK_COUNT)
 
@@ -79,10 +79,10 @@ func test_update_item_multiple_stack_overflow() -> void:
 	self.inventory_manager = auto_free(InventoryManager.new().with_inventory(null))
 	var item_stack_1: ItemStack = InventoryHelpers.build_test_item_stack()
 	item_stack_1.count = InventoryHelpers.DEFAULT_MAX_STACK_SIZE
-	assert_that(self.inventory_manager.add_item_stack(item_stack_1)).is_null()
+	assert_that(self.inventory_manager._add_item_stack(item_stack_1)).is_null()
 	var item_stack_2: ItemStack = InventoryHelpers.build_test_item_stack()
 	item_stack_2.count = InventoryHelpers.DEFAULT_MAX_STACK_SIZE * 2
-	assert_that(self.inventory_manager.add_item_stack(item_stack_2)).is_null()
+	assert_that(self.inventory_manager._add_item_stack(item_stack_2)).is_null()
 	assert_that(self.inventory_manager.inventory_data.inventory_items[2].count) \
 			.is_equal(InventoryHelpers.DEFAULT_MAX_STACK_SIZE)
 	assert_that(self.inventory_manager.inventory_data.inventory_metadata_map[item_stack_2.item.id] \
