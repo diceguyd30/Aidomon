9f1e8c35a3d7e046d76cc0b87a381a83341f8c22
diceguyd30 <michael@ennismightier.com>
2024-08-18 20:36:23 -0700

    Add inventory update connection

--- scenes/craftbench/crafting_menu_item.gd
+++ scenes/craftbench/crafting_menu_item.gd
@@ -14,7 +14,7 @@ const BIG_ITEM_STACK_UI = preload("res://scenes/common/big_item_stack_ui.tscn")
 const ITEM_STACK_UI = preload("res://scenes/common/item_stack_ui.tscn")
 
 func _ready() -> void:
-	
+	GameSignals.update_inventory_signal.connect(_update_cost_colors)
 	_update()
 	
 func _update() -> void:
@@ -29,6 +29,9 @@ func _update() -> void:
 	if cost_grid != null:
 		for item: ItemStack in recipe.cost.item_list:
 			cost_grid.add_child(ITEM_STACK_UI.instantiate().with_data(item))
+			
+func _update_cost_colors() -> void:
+	pass
 
 func _on_button_pressed() -> void:
 	GameSignals.reward_player(recipe.reward)
