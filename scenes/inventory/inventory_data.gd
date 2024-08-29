fc1fa7e8c5504447af7a050cdbf2891efa6c3582
diceguyd30 <michael@ennismightier.com>
2024-08-28 18:13:01 -0700

    InventoryData as a resource needs work...
    TODO: use setters to invoke a function to auto-create the metadata map so this can actually be used from the editor

--- scenes/inventory/inventory_data.gd
+++ scenes/inventory/inventory_data.gd
@@ -1,16 +1,16 @@
 class_name InventoryData
-extends Resource
+extends RefCounted
 
-@export var max_inventory_size: int = 10
+var max_inventory_size: int = 10
 # Ordered inventory items as they should appear in the UI
-@export var inventory_items: Array[ItemStack] = []
+var inventory_items: Array[ItemStack] = []
 # Dictionary<ItemID: _InventoryMetadata>
-@export var inventory_metadata_map: Dictionary = {}
+var inventory_metadata_map: Dictionary = {}
 
 class _InventoryMetadata:
-	@export var item_count: int = 0
+	var item_count: int = 0
 	# Dictionary<inventory_index: true>  Used as a Set
-	@export var index_map: Dictionary
+	var index_map: Dictionary
 
 func with_initialized_inventory(max_inventory_size_: int = -1) -> InventoryData:
 	if max_inventory_size_ > 0:
