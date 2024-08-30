1e6c674568799a14838c52f14ef45fdf189c7eaf
diceguyd30 <michael@ennismightier.com>
2024-08-30 09:43:52 -0700

    Making a generic town location
    This scene will be responsible for handling the logic of unlocking town locations for interaction. Part of $11w

---
+++ town_location.gd
@@ -0,0 +1,7 @@
+class_name TownLocation
+extends Control
+
+@onready var location_container: VBoxContainer = %LocationContainer
+@onready var location_btn: TextureButton = %LocationBtn
+@onready var location_lbl: Label = %LocationLbl
+@onready var cost_container: CenterContainer = %CostContainer
