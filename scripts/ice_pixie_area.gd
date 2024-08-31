c0b530e5973faa65232a8188ef7622241eecad5b
diceguyd30 <michael@ennismightier.com>
2024-08-30 21:34:52 -0700

    Prepping for adding creatures

--- scripts/ice_pixie_area.gd
+++ scripts/ice_pixie_area.gd
@@ -1,12 +1,9 @@
 extends Area2D
 
 signal clicked
-@onready var ice_pixie = %ice_pixie
+@onready var ice_pixie: AnimatedSprite2D = %ice_pixie
 
-func _process(delta):
-	ice_pixie.position.x -= 5 * delta
-
-func _on_input_event(_viewport, event, _shape_idx):
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if Input.is_action_just_pressed("click", true) and event.is_pressed() and not event.is_echo():
 		print("You Clicked Me!")
-		emit_signal("clicked")
+		clicked.emit()
