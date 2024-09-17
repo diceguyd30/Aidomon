f33ee38045ed3fc025ebfc4fc91498ae981f023b
diceguyd30 <michael@ennismightier.com>
2024-09-17 12:44:07 -0700

    Creating a new Wilderness scene
    Part of $129

--- scenes/wilderness/env_interaction_ui.gd
+++ scenes/wilderness/env_interaction_ui.gd
@@ -13,6 +13,8 @@ extends Control
 @onready var reward_grid: GridContainer = %RewardGrid
 
 func _ready() -> void:
+	if !Engine.is_editor_hint():
+		GameSignals.player_assigned_to_work.connect(_unassign_from_work)
 	_update()
 
 func _update() -> void:
@@ -39,11 +41,15 @@ func _process(_delta: float) -> void:
 		progress_bar.value = ((timer.wait_time - timer.time_left) / timer.wait_time) * 100
 
 func _on_timer_timeout() -> void:
+	if !Engine.is_editor_hint():
+		GameSignals.reward_player(environment_item.reward)
 	timer.start()
 
-func _on_btn_collect_toggled(toggled_on: bool) -> void:
-	if toggled_on:
-		timer.start()
-	else:
-		progress_bar.value = 0
-		timer.stop()
+func _on_btn_collect_pressed() -> void:
+	if !Engine.is_editor_hint():
+		GameSignals.player_being_assigned_to_work()
+	timer.start()
+
+func _unassign_from_work() -> void:
+	progress_bar.value = 0
+	timer.stop()
--- state/game_signals.gd
+++ state/game_signals.gd
@@ -2,9 +2,13 @@ extends Node
 
 signal reward_player_signal(reward: ItemBundle)
 signal update_inventory_signal
+signal player_assigned_to_work
 
 func reward_player(reward: ItemBundle) -> void:
 	reward_player_signal.emit(reward)
 
 func inventory_updated() -> void:
 	update_inventory_signal.emit()
+
+func player_being_assigned_to_work() -> void:
+	player_assigned_to_work.emit()
