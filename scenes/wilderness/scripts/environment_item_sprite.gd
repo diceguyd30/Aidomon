42752b09244807eb3f910a38e3977481d003ec04
diceguyd30 <michael@ennismightier.com>
2024-10-05 21:42:02 -0700

    Removing more old wilderness scripts

--- scenes/wilderness/scripts/environment_item_sprite.gd
+++
@@ -1,8 +0,0 @@
-extends Sprite2D
-
-signal clicked
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("click"):
-		if is_pixel_opaque(get_local_mouse_position()):
-			clicked.emit()
