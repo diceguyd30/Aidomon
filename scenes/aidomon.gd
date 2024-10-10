45ab4345feee8fa329d6642546e6f708305b367f
diceguyd30 <michael@ennismightier.com>
2024-10-09 20:59:34 -0700

    Added a fullscreen keybind

--- scenes/aidomon.gd
+++ scenes/aidomon.gd
@@ -13,3 +13,10 @@ func _on_inventory_btn_pressed() -> void:
 func _on_aidomon_btn_pressed() -> void:
 	collection.visible = true
 	inventory.visible = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("fullscreen") and event.is_action("fullscreen"):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
