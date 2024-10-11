72f816d37eaaa71b81719b0ed60b08712e866682
diceguyd30 <michael@ennismightier.com>
2024-10-11 15:51:21 -0700

    More panel visibility adjustments

--- scenes/biomes/starting_town.gd
+++ scenes/biomes/starting_town.gd
@@ -1,11 +1,11 @@
 class_name StartingTown
 extends Control
 
-@onready var wilderness_panel: PanelContainer = %WildernessPanel
-@onready var craftbench_panel: PanelContainer = %CraftbenchPanel
+@onready var wilderness_container: MarginContainer = %WildernessContainer
+@onready var craftbench_container: MarginContainer = %CraftbenchContainer
 
 func _on_wilderness_map_location_toggled(toggled_on: bool) -> void:
-	wilderness_panel.visible = toggled_on
+	wilderness_container.visible = toggled_on
 
 func _on_craftbench_map_location_toggled(toggled_on: bool) -> void:
-	craftbench_panel.visible = toggled_on
+	craftbench_container.visible = toggled_on
