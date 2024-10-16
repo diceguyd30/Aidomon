298765e517ebf5dc26fec2f9fb3dccd37c1232e7
diceguyd30 <michael@ennismightier.com>
2024-10-15 21:40:04 -0700

    Set biome data in scene
    We'll have to figure out how to set the interactions panel background.

--- scenes/biomes/starting_town.gd
+++ scenes/biomes/starting_town.gd
@@ -1,6 +1,11 @@
 class_name StartingTown
 extends Control
 
+@export var biome_data: BiomeData:
+	set(value):
+		biome_data = value
+		_update()
+
 @onready var wilderness_container: MarginContainer = %WildernessContainer
 @onready var craftbench_container: MarginContainer = %CraftbenchContainer
 
@@ -9,3 +14,6 @@ func _on_wilderness_map_location_toggled(toggled_on: bool) -> void:
 
 func _on_craftbench_map_location_toggled(toggled_on: bool) -> void:
 	craftbench_container.visible = toggled_on
+
+func _update() -> void:
+	pass
