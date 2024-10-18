579174587c3558f25ac64f37e3f37223740e3d35
diceguyd30 <michael@ennismightier.com>
2024-10-18 12:54:46 -0700

    Update main_menu.gd
    Load from Packed Scene instead of a string path

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -1,13 +1,13 @@
 class_name MainMenu
 extends Control
 
-const AIDOMON_PATH: String = "res://scenes/aidomon.tscn"
+const AIDOMON = preload("res://scenes/aidomon.tscn")
 
 func _on_continue_btn_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_new_game_btn_pressed() -> void:
-	get_tree().change_scene_to_file(AIDOMON_PATH)
+	get_tree().change_scene_to_packed(AIDOMON)
 
 func _on_load_game_btn_pressed() -> void:
 	pass # Replace with function body.
