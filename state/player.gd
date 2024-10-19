d53f07c4b4101d4300a6970f45da98dc4d94472d
diceguyd30 <michael@ennismightier.com>
2024-10-19 14:35:16 -0700

    Update player.gd

--- state/player.gd
+++ state/player.gd
@@ -2,7 +2,11 @@ extends Node
 
 const Constants = preload("res://scripts/constants.gd")
 
-var player_inventory_size: int = 10
+var player_inventory_size: int = 10:
+	set(value):
+		player_inventory_size = value
+		player_inventory.max_inventory_size = player_inventory_size
+
 var player_inventory: InventoryData = InventoryData.new().of_size(player_inventory_size)
 var biome_unlocks: BiomeUnlocks = BiomeUnlocks.new()
 var aidomon_collection: Array[Creature] = []
