02368d89675af5110f2ce0b2c206c93f44229626
diceguyd30 <michael@ennismightier.com>
2024-11-03 18:00:42 -0800

    Add a subgroup for level range

--- resources/biomes/biome_data.gd
+++ resources/biomes/biome_data.gd
@@ -5,5 +5,9 @@ extends Resource
 @export var name: String
 @export var background: Texture
 @export var environment_items: Array[EnvironmentItemData] = []
+
+@export_subgroup("Creature Encounters")
 # Map of Creature to probability of the creature's appearance in the wilderness.
 @export var native_creatures: Dictionary[SpeciesData, float] = {}
+@export var min_creature_level: int
+@export var max_creature_level: int
